7ae3d6b4602f98e767fce5cef4757a3ccebb7a87
cameronmi <23000876+cameronmi@users.noreply.github.com>
2025-04-10 02:10:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,4 +24,65 @@
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
+    var urlDom = document.getElementById('inputURL');
+    var nameCheckDom = document.getElementById('nameCheck');
+    var nicknameDom = document.getElementById('inputNickname');
+    var sizeDom = document.getElementById('inputSize');
+    var radioCSSDom = document.getElementById('radioCSS');
+    var radioJSDom = document.getElementById('radioJS');
+    var outputDom = document.getElementById('output');
+    var styleDom =  document.getElementById('customStyle');
+    
+    urlDom.onchange = onEdit;
+    nameCheckDom.onchange = function (e) {
+        if (e.target.checked) {
+            nicknameDom.disabled = false;
+        } else {
+            nicknameDom.disabled = true;
+        }
+        onEdit();
+    };
+    nicknameDom.onchange = onEdit;
+    sizeDom.onchange = onEdit;
+    radioCSSDom.onchange = onEdit;
+    radioJSDom.onchange = onEdit;
+
+    function onEdit() {
+        var size = sizeDom.valueAsNumber;
+        var nickname = nicknameDom.value;
+
+        var styles = `
+.group\\/conversation-turn.agent-turn:before {
+    content: " ";
+    display: block;
+    background-image: url("${urlDom.value}");
+    background-size: ${size}px ${size}px;
+    border-radius: ${size / 2}px;
+    height: ${size}px;
+    width: ${size}px;
+}
+.group\\/conversation-turn.agent-turn > div:nth-child(1):before {
+    content: "${nameCheckDom.checked ? nickname : " "}";
+    display: block;
+    position: relative;
+    left: ${size+10}px;
+    top: ${nameCheckDom.checked ? -5: 0}px;
+    font-weight: 500;
+}
+.group\\/conversation-turn.agent-turn > div:nth-child(1) {
+    margin-top: ${nameCheckDom.checked ? -21: 0}px;
+}
+        `
+
+        var jsCode = `var style=document.createElement('style');style.innerHTML='${styles.trim().replaceAll('\n', '').replace('\\', '\\\\')}';document.head.appendChild(style);`
+        jsCode = `javascript:(function(){${jsCode}})()`;
+        if (radioCSSDom.checked) {
+            outputDom.getElementsByTagName('code')[0].innerText = styles;
+        } else {
+            outputDom.getElementsByTagName('code')[0].innerText = jsCode;
+        }
+        styleDom.innerHTML = styles;
+        console.log(styles);
+    };
 })(document);
+
